1d1065df2df2b63fc11a498bd95f23fb93d3f614
Jazzcort <120679580+Jazzcort@users.noreply.github.com>
2024-06-30 14:46:43 -0400

    Test

--- src/main.rs
+++ src/main.rs
@@ -15,29 +15,16 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                // let mut req = String::new();
-                // if let Ok(_) = stream.read_to_string(&mut req) {
-                //     stream.write(b"+PONG\r\n").unwrap();
-                //     let a: Vec<&str> = req.split("\r\n").collect();
-                //     match a[2].to_lowercase().as_str() {
-                //         // "ping" => {
-                //         //     let response = String::from("+PONG\r\n");
-                //         //     stream.write(response.as_bytes()).unwrap();
-
-                //         // }
-                //         _ => {
-                //             // let response = String::from("+PONG\r\n");
-                //             stream.write(b"+PONG\r\n").unwrap();
-                //         }
-                //     }
-                // }
                 println!("accepted new connection");
 
                 let mut buf = [0; 512];
-
-                stream.read(&mut buf).unwrap();
-
-                stream.write(b"+PONG\r\n").unwrap();
+                loop {
+                    let read_count = stream.read(&mut buf).unwrap();
+                    if read_count == 0 {
+                        break;
+                    }
+                    stream.write(b"+PONG\r\n").unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);

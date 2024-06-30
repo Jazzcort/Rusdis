07cc5997af74ee2a40d7a5b3ce4b50abb43e6df4
Jazzcort <120679580+Jazzcort@users.noreply.github.com>
2024-06-30 14:07:09 -0400

    Added Ping

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,7 +14,21 @@ fn main() {
     //
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
+                let mut req = String::new();
+                if let Ok(_) = stream.read_to_string(&mut req) {
+                    let a: Vec<&str> = req.split("\r\n").collect();
+                    match a[2] {
+                        "PING" | "ping" => {
+                            let response = String::from("+PONG\r\n");
+                            stream.write(response.as_bytes()).unwrap();
+                            
+                        }
+                        _ => {
+                            
+                        }
+                    }
+                }
                 println!("accepted new connection");
             }
             Err(e) => {

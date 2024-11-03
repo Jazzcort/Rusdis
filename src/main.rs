5686d4392513d145b235566ab979401b6606b546
Jazzcort <120679580+Jazzcort@users.noreply.github.com>
2024-11-02 21:07:57 -0400

    Finished multiple commands feature

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,72 @@
 // Uncomment this block to pass the first stage
-use std::{
-    io::{Read, Write},
-    net::TcpListener,
-};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::task;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
     //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     //
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
 
-                let mut buf = [0; 512];
-                loop {
-                    let read_count = stream.read(&mut buf).unwrap();
-                    if read_count == 0 {
-                        break;
-                    }
-                    stream.write(b"+PONG\r\n").unwrap();
-                }
+    loop {
+        let res = listener.accept().await;
+
+        match res {
+            Ok((stream, _)) => {
+                println!("accepted new connection");
+                task::spawn(async move {
+                    handle_commands(stream).await;
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+    //for res in listener.accept().await {
+    //    match res {
+    //        Ok((mut stream, _)) => {
+    //            println!("accepted new connection");
+    //
+    //            let mut buf = [0; 512];
+    //            loop {
+    //                let read_count = stream.read(&mut buf).unwrap();
+    //                if read_count == 0 {
+    //                    break;
+    //                }
+    //
+    //                stream.write(b"+PONG\r\n").unwrap();
+    //            }
+    //        }
+    //        Err(e) => {
+    //            println!("error: {}", e);
+    //        }
+    //    }
+    //}
+}
+
+async fn handle_commands(mut stream: TcpStream) -> Result<(), std::io::Error> {
+    let (reader, writer) = stream.split();
+    let mut reader = BufReader::new(reader);
+    let mut buf = Vec::from(reader.fill_buf().await?);
+    reader.consume(buf.len());
+
+    let commands = String::from_utf8_lossy(&buf);
+    let commands = commands.split("\n");
+
+    for command in commands {
+        match command {
+            "ping" | "PING" => {
+                stream.write_all(b"+PONG\r\n").await?;
+            }
+            _ => {}
+        }
+    }
+
+    Ok(())
 }

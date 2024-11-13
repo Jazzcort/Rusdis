aa93fb529fc4e98991cb689de6fe70ad3ef94068
Jazzcort <120679580+Jazzcort@users.noreply.github.com>
2024-11-13 18:43:47 -0500

    Started RDB file

--- src/rdb_file_reader.rs
+++ src/rdb_file_reader.rs
@@ -7,11 +7,14 @@ pub fn read_rdb(f_path: String) -> Result<(), RusdisError> {
     let f = File::open(f_path)?;
     let mut reader = BufReader::new(f);
 
-    let mut line = String::new();
+    //let mut line = String::new();
+    let mut buf: [u8] = [0; 512];
+    reader.read(&mut buf);
+    dbg!(buf);
 
-    while reader.read_line(&mut line)? > 0 {
-        dbg!(&line);
-    }
+    //while reader.read_line(&mut line)? > 0 {
+    //    dbg!(&line);
+    //}
 
     Ok(())
 }

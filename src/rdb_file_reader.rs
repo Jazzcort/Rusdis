8d722ec65b80250fac3cbc63a90b415ee336b77b
Jazzcort <120679580+Jazzcort@users.noreply.github.com>
2024-11-13 22:55:19 -0500

    Started RDB file

--- src/rdb_file_reader.rs
+++ src/rdb_file_reader.rs
@@ -2,14 +2,42 @@ use crate::error::RusdisError;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
+use std::time::Instant;
+
+pub enum Phase {
+    MagicString,
+    RDBVersion,
+    AuxiliaryField,
+    DBSelector,
+    ResizedbField,
+    KeyValue,
+    CheckSum,
+}
+
+pub struct RDBFile {
+    pub rdb_version: String,
+    pub redis_bits: usize,
+    pub redis_ver: usize,
+    pub datasets: Vec<Dataset>,
+}
+
+pub struct Dataset {
+    pairs: Vec<(String, ValueType)>,
+    expirations: Vec<Instant>,
+}
+
+pub enum ValueType {
+    String(String),
+}
 
 pub fn read_rdb(f_path: String) -> Result<(), RusdisError> {
     let f = File::open(f_path)?;
     let mut reader = BufReader::new(f);
 
+    let mut buf = vec![];
+
+    let length = reader.read_to_end(&mut buf)?;
     //let mut line = String::new();
-    let mut buf: [u8; 128] = [0; 128];
-    reader.read(&mut buf);
     dbg!(&buf);
     dbg!(String::from_utf8_lossy(&buf));
 

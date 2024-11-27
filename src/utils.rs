f63e20b8039f64d217f7871ac5833456852f74b1
Jazzcort <120679580+Jazzcort@users.noreply.github.com>
2024-11-26 20:40:58 -0500

    started replid

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,8 @@
-//use rand::{distributions::Alphanumeric, Rng};
-//pub(crate) fn generate_random_string(length: usize) -> String {
-//    rand::thread_rng()
-//        .sample_iter(&Alphanumeric)
-//        .take(length)
-//        .map(char::from)
-//        .collect::<String>()
-//}
+use rand::{distributions::Alphanumeric, Rng};
+pub(crate) fn generate_random_string(length: usize) -> String {
+    rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(length)
+        .map(char::from)
+        .collect::<String>()
+}
